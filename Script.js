d87837c8052d71a2efd25f0b1c85b5b8cf25112e
Fredgred-eie <tonyemady1@gmail.com>
2026-05-23 12:11:51 +0800

    Javasiprture

---
+++ Script.js
@@ -0,0 +1,14 @@
+function showPopup() {
+  let popup = document.getElementById("popup");
+
+  // slide in
+  popup.style.right = "20px";
+
+  // slide out after 3 seconds
+  setTimeout(() => {
+    popup.style.right = "-550px";
+  }, 3000);
+}
+
+// repeat every 10 seconds
+setInterval(showPopup, 10000);
